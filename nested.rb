e8c1fcabeb3fbc7d46a59bb2fe55b36c38bb4206
Ryan Schmelter <rschmelter33@gmail.com>
2017-10-29 19:15:31 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -81,8 +81,13 @@ def adding_matz
           :languages => ["C"]
         }
      }
+		 programmer_hash[:yukihiro_matsumoto]
+		 programmer_hash[:yukihiro_matsumoto][:known_for]
+		 programmer_hash[:yukihiro_matsumoto][:languages]
 		 programmer_hash[:yukihiro_matsumoto][:known_for] = "ruby"
 		 programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
+
+		 programmer_hash[:yukihiro_matsumoto]
 
 end
 
